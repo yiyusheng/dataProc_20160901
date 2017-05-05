a5127eed50ca268a34282f3c2dce214d8b2449fb
yorkyi <yiyusheng.hust@gmail.com>
2017-05-05 17:16:26 +0800

    0505:commit

--- base_var.R
+++ base_var.R
@@ -3,8 +3,8 @@ dirName <- 'Load_Data_2015'
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7","#CC6666", "#9999CC", "#66CC99","#000000")
 attrNameAll <- c('util','rps','iopsr','wps','iopsw');attrNameDis <- attrNameAll[c(2,4,1)]
 attrNameExtend <- c('util','rps','wps',paste('iopsr_',1:24,sep=''),paste('iopsw_',1:24,sep=''))
-dir_code <- paste(dir_c,dirName,sep='')
-dir_data <- paste(dir_d,dirName,sep='')
+dir_code <- file.path(dir_c,dirName)
+dir_data <- file.path(dir_d,dirName)
 
 if (osFlag){
   source('D:/Git/R_libs_user/R_custom_lib.R')
